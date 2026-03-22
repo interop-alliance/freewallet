c70f46138ba3f4a995718ef17c625a820a2e10c1
Dmitri Zagidulin <dzagidulin@gmail.com>
2026-03-21 21:37:14 -0400

    Set up directory aliasing in config.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true
   },
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
