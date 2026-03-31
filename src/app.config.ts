12d3c74a8bc5780429a50b57d2d0d76fa851ce57
Omar Salah <omar.salah1597@gmail.com>
2026-03-31 15:48:54 +0200

    Update app configuration to export BASE_URL and WAS_SERVER_URL from environment variables

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,7 +3,8 @@
  */
 const env = import.meta.env
 
-export const WAS_SERVER_URL = env.VITE_WAS_SERVER_URL
+export const BASE_URL = env.BASE_URL
+export const WAS_SERVER_URL = env.WAS_SERVER_URL
 
 export const PASSWORD_RULES = {
   minlength: 16,
